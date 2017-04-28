285d9943a4ef39ff95a6300da500c7bbb5ae1e29
Colton Kopsa <coljamkop@gmail.com>
2017-04-28 14:39:50 -0600

    Made an update...

--- lb01/tk1/WebServer.java
+++ lb01/tk1/WebServer.java
@@ -60,28 +60,28 @@ final class HttpRequest implements Runnable {
     private void processRequest() throws Exception {
         // Get a reference to the socket's input and output streams.
         InputStream is = socket.getInputStream();
-        DataOutputStream os = socket.getOutputStream();
+        DataOutputStream os = new DataOutputStream(socket.getOutputStream());
 
         // Set up input stream filters.
         //??
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+	BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-            // Get the request line of the HTTP request message.
-            String requestLine = br.readLine();
+	// Get the request line of the HTTP request message.
+	String requestLine = br.readLine();
 
-            // Display the request line.
-            System.out.println();
-            System.out.println(requestLine);
+	// Display the request line.
+	System.out.println();
+	System.out.println(requestLine);
 
-            // Get and display the header lines.
-            String headerLine = null;
-            while ((headerLine = br.readLine()).length() != 0) {
-                System.out.println(headerLine);
-            }
+	// Get and display the header lines.
+	String headerLine = null;
+	while ((headerLine = br.readLine()).length() != 0) {
+	    System.out.println(headerLine);
+	}
 
-            // Close streams and socket.
-            os.close();
-            br.close();
-            socket.close();
+	// Close streams and socket.
+	os.close();
+	br.close();
+	socket.close();
     }
 }
